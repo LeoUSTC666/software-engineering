1fae585fc87f5dcad600d5ae8697beedd88e030c
TLDuo <1035592565@qq.com>
2024-12-07 20:03:39 +0800

    test

--- sql_src/func.py
+++ sql_src/func.py
@@ -11,7 +11,7 @@ import pymysql
 
 def get_db_connection():
     try:
-        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='leo520', db='lab2', charset='utf8')
+        conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123456', db='evalution_system', charset='utf8')
         return conn
     except pymysql.MySQLError as e:
         print(f"Error connecting to the database: {e}")
